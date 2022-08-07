93cb0453f2ce07f4497df03bb1dafc50cbb494bb
Christina McIntyre <christinamcintyre1@gmail.com>
2022-08-06 20:06:14 -0600

    added form

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { useFormik } from "formik";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const SignupForm = () => {
+  const formik = useFormik({
+    initialValues: { email: "" },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="source">Date Applied</label>
+      <input
+        id="date"
+        name="date"
+        type="date"
+        onChange={formik.handleChange}
+        value={formik.values.date}
+      />
+      <label htmlFor="name">Company Name</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+       <label htmlFor="position">Position</label>
+      <input
+        id="position"
+        name="position"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.position}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+       <label htmlFor="source">Source</label>
+      <input
+        id="source"
+        name="source"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.source}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return <SignupForm />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
